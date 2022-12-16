686e20150c08c756f10de07fbf8ee7b6525d715d
ShristyBhati21 <shristy.bhati@byzgen.com>
2022-12-16 15:28:56 +0000

    handle failure status

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,19 +4,19 @@
 # This email agent takes the values for the flags from the workflow where it is used.
 #
 
+git config --global --add safe.directory '/github/workspace'
+
 if [ "${INPUT_JOB_STATUS}" = 'success' ]
 then
   export INPUT_DISPLAY_IMAGE_URL='https://media.giphy.com/media/ekwEeLxb7G4DW44YaK/giphy.gif'
+  export TAG_VERSION=$(git describe --tag --abbrev=0)
 
 elif [ "${INPUT_JOB_STATUS}" = 'failure' ]
 then
   export INPUT_DISPLAY_IMAGE_URL='https://media.giphy.com/media/xUNd9TXtXVONOxbQ1W/giphy.gif'
+  export TAG_VERSION='No release yet'
 
 fi # else cancelled
-
-#Retrieving latest tag version
-git config --global --add safe.directory '/github/workspace'
-export TAG_VERSION=$(git describe --tag --abbrev=0)
 
 envsubst < /message_template.html > /message.html
 
